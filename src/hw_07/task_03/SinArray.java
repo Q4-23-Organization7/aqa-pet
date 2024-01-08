fa8339de4d61e317d68f5b7ced700fea3672e016
Oleksandr Alieksieiev <alexeev.army@gmail.com>
2024-01-08 15:13:20 +0200

    Third Commit

--- src/hw_07/task_03/SinArray.java
+++ src/hw_07/task_03/SinArray.java
@@ -1,4 +1,17 @@
 package hw_07.task_03;
 
 public class SinArray {
+
+    public static int x = 0;
+
+    public static void main(String[] args) {
+
+        double[] mas = new double[36];
+
+        for (int i = 0; i < 36; i++) {
+            mas[i] = Math.sin(Math.toRadians(x));
+            System.out.println("Sin(x) = " + mas[i]);
+            x = x + 10;
+        }
+    }
 }
