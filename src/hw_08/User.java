de0441d159b7941cbea2853ce61b3d30977240be
Oleksandr Alieksieiev <alexeev.army@gmail.com>
2024-01-10 21:55:27 +0200

    First Commit

---
+++ src/hw_08/User.java
@@ -0,0 +1,65 @@
+package hw_08;
+
+public class User {
+
+    public String userName;
+    private int age;
+    public String email;
+    private String password;
+    public boolean isActive;
+    public double amountSpentMoney;
+
+    public User() {
+
+    }
+
+    public User(String userName) {
+        this.userName = userName;
+    }
+
+    public User(String userName, String password) {
+        this.userName = userName;
+        this.password = password;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        if (password.length() >= 5) {
+            this.password = password;
+        } else {
+            System.out.println("Error. Your password is to little");
+        }
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        if (age >= 0) {
+            this.age = age;
+        } else {
+            System.out.println("Error. Age cannot be negative number");
+        }
+    }
+
+
+    public void makePurchase(double purchasePrice) {
+        amountSpentMoney = amountSpentMoney + purchasePrice;
+    }
+
+    public void printTotalAmountOfSpentMoney() {
+        System.out.println("The total amount of money spent is: " + amountSpentMoney);
+    }
+
+    public void printData() {
+        System.out.println("The name of User is: " + userName +
+                "\nUser age: " + getAge() +
+                "\nUser email: " + email +
+                "\nUser Password: " + getPassword() +
+                "\nActive User: " + isActive);
+    }
+}
---
+++ src/hw_08/UserTest.java
@@ -0,0 +1,55 @@
+package hw_08;
+
+public class UserTest {
+
+        public static void main(String[] args) {
+
+            User firstUser = new User();
+            firstUser.userName = "Tom";
+            firstUser.setAge(25);
+            firstUser.email = "test@test.com";
+            firstUser.setPassword("qwerty");
+            firstUser.isActive = true;
+            firstUser.makePurchase(100);
+            firstUser.makePurchase(350);
+
+
+            User secondUser = new User();
+            secondUser.userName = "Tom";
+            secondUser.setAge(25);
+            secondUser.email = "demo@demo.com";
+            secondUser.setPassword("qwerty");
+            secondUser.isActive = true;
+            secondUser.makePurchase(150);
+            secondUser.makePurchase(250);
+
+            User thirdUser = new User("Rex");
+            thirdUser.setAge(45);
+            thirdUser.email = "email@email.com";
+            thirdUser.setPassword("string");
+            thirdUser.isActive = false;
+            thirdUser.makePurchase(50);
+            thirdUser.makePurchase(550);
+
+            User nextUser = new User("Bob", "password");
+            nextUser.setAge(55);
+            nextUser.email = "gmail@gmail.com";
+            nextUser.isActive = true;
+            nextUser.makePurchase(99);
+            nextUser.makePurchase(55);
+
+            firstUser.printData();
+            firstUser.printTotalAmountOfSpentMoney();
+            System.out.println("----------------------");
+            secondUser.printData();
+            secondUser.printTotalAmountOfSpentMoney();
+            System.out.println("----------------------");
+            thirdUser.printData();
+            thirdUser.printTotalAmountOfSpentMoney();
+            System.out.println("----------------------");
+            nextUser.printData();
+            nextUser.printTotalAmountOfSpentMoney();
+
+
+        }
+}
