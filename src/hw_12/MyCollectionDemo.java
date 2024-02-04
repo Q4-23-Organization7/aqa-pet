af7e07c8eeea8ac33ad389a25afc9b11dc43f6f2
Oleksandr Alieksieiev <alexeev.army@gmail.com>
2024-02-04 20:21:28 +0200

    First Commit

---
+++ src/hw_12/MyCollectionDemo.java
@@ -0,0 +1,11 @@
+package hw_12;
+
+import static hw_12.MyCollection.createList;
+
+    public class MyCollectionDemo {
+
+        public static void main (String[] args){
+            System.out.println(createList());
+        }
+    }
+
